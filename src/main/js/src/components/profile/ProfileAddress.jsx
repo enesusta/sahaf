8c4ba1c24d110a787ac404187f985f97bef6d480
Enes Usta <enesusta@email.com>
2020-07-06 03:33:08 +0300

    Add custom router on Profile.jsx and other relevant components

---
+++ src/main/js/src/components/profile/ProfileAddress.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ProfileAddress = () => {
+
+    return <li>
+        address
+    </li>
+};
+
+export default ProfileAddress;
---
+++ src/main/js/src/components/profile/ProfilePasswordChanger.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfilePasswordChanger = () => {
+
+    return (
+        <li>
+            'sifre degis
+        </li>
+    )
+};
+
+export default ProfilePasswordChanger;
